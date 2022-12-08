615e8615f5f0897cacaaefc55ac483f97da88c58
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-08 15:24:15 +0100

    feat: add get-request-duration

---
+++ utils/get-request-duration.js
@@ -0,0 +1,7 @@
+module.exports = function getRequestDuration(startTime) {
+  const NS_PER_SEC = 1e9
+  const NS_TO_MICRO = 1e3
+  const diff = process.hrtime(startTime)
+  const microseconds = (diff[0] * NS_PER_SEC + diff[1]) / NS_TO_MICRO
+  return Math.ceil(microseconds)
+}
