f52c7a29d381919d85a0c18c7dee4373a73373a7
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2022-12-08 15:25:38 +0100

    feat: add validate-treblle-payload

---
+++ utils/validate-treblle-payload.js
@@ -0,0 +1,18 @@
+const Validator = require('jsonschema').Validator
+const v = new Validator()
+const trebllePayloadSchema = require('../treblle-payload-schema.json')
+
+/**
+ *
+ * @param {object} payload
+ */
+module.exports = function validateTrebllePayload(payload) {
+  const response = v.validate(payload, trebllePayloadSchema)
+  if (response.valid) return response.valid
+  const payloadError = new Error({
+    name: 'Invalid Treblle payload',
+    message: 'Verify that the Treblle payload you are sending is valid.',
+  })
+  payloadError.validationErrors = response.errors
+  throw payloadError
+}
